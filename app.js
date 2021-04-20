3a834aeb69f5bd5d494d8921984f31168c09d7b7
Kwon_Jingu <wlsrndmswls1@naver.com>
2021-04-20 21:07:24 +0900

    4/20

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const canvas = document.getElementById("jsCanvas");
 
+let painting = false;
+
+
 function onMouseMove(event){
-    console.log(event);     
+    const x = event.offsetX;
+    const y = event.offsetY;
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+function onMouseUp(event){
+    stopPainting()
 }
 
 if(canvas){
-    canvas.addEventListener("mousemove",on,onMouseMove)
+    canvas.addEventListener("mousemove",onMouseMove);
+    ca nvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener
 }
+  
+    //2-1 공부가 재미가 없어요

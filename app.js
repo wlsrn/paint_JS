a32489468a58852279f5dc044d1ab34dcf80fb29
Kwon_Jingu <wlsrndmswls1@naver.com>
2021-04-26 21:17:31 +0900

    4/26

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-
+canvas.width = 700;
+canvas.height = 700;
 ctx.strokeStyle ="#2c2c2c";
 ctx.lineWidth = 2.5;
+
 let painting = false;
 
 
@@ -18,6 +20,14 @@ function startPainting(){
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }
+    else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
@@ -34,4 +44,3 @@ if(canvas){
     canvas.addEventListener("mouseup", onMouseUp);
     canvas.addEventListener
 }
-

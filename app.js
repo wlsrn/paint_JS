606b14e231a84654b430a9923172ad610ede9ddb
Kwon_Jingu <wlsrndmswls1@naver.com>
2021-04-28 23:38:54 +0900

    4/28 finish PrintJs

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.fillStyle = INITIAL_COLOR;
+ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+
 let painting = false;
 let filling = false;
-
-let save = false;
 
 function stopPainting() {
     painting = false;
@@ -34,6 +41,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }  
 
 function handleRangeChange(event){
@@ -53,11 +61,30 @@ function handleModeChange(event){
 
 }
 
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+function handleCM(event){
+    event.preventDefault()
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/jped");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJs    ";
+    link.click();
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
 Array.from(colors).forEach(color =>
@@ -75,4 +102,6 @@ if(mode){
     mode.addEventListener("click",handleModeChange)
 }
 
-//2.5
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick)
+}
